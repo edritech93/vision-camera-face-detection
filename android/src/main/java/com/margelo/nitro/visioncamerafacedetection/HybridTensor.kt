79bba14f165ae9ed29d22cb28411e198c778c9bf
Yudi Alviska <yudi.a@weefer.co.id>
2026-05-07 10:29:25 +0700

    feat: implement HybridTensor for face detection and add VisionCameraFaceDetectionPlugin

--- android/src/main/java/com/margelo/nitro/visioncamerafacedetection/HybridTensor.kt
+++ android/src/main/java/com/margelo/nitro/visioncamerafacedetection/HybridTensor.kt
@@ -1,23 +1,19 @@
-package com.visioncamerafacedetection
+package com.margelo.nitro.visioncamerafacedetection
 
 import android.content.res.AssetManager
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Matrix
 import android.graphics.RectF
 import android.util.Base64
+import androidx.core.graphics.createBitmap
+import com.facebook.proguard.annotations.DoNotStrip
 import com.facebook.react.bridge.Arguments
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.WritableMap
 import com.facebook.react.bridge.WritableNativeMap
 import com.google.android.gms.tasks.Tasks
-import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.FaceDetection
-import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.margelo.nitro.NitroModules
 import org.tensorflow.lite.Interpreter
 import java.io.FileInputStream
 import java.io.IOException
@@ -26,8 +22,10 @@ import java.nio.FloatBuffer
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel
 
-class VisionCameraFaceDetectionModule(private val reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
+@DoNotStrip
+class HybridTensor() : HybridTensorSpec() {
+
+  private val context = NitroModules.applicationContext ?: throw Error("Image Face Detector - No Context available!")
 
   private var faceDetectorOptions = FaceDetectorOptions.Builder()
     .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
@@ -38,34 +36,24 @@ class VisionCameraFaceDetectionModule(private val reactContext: ReactApplication
 
   private var faceDetector = FaceDetection.getClient(faceDetectorOptions)
 
-  @ReactMethod
-  private fun initTensor(modelFile: String = "mobile_face_net", count: Int = 1, promise: Promise) {
+  override fun initTensor(modelPath: String, count: Double?): String {
+    // TODO("Not yet implemented")
     try {
-      val assetManager = reactContext.assets
-      val byteFile: MappedByteBuffer = loadModelFile(assetManager, modelFile)
+      val assetManager = context.assets
+      val byteFile: MappedByteBuffer = loadModelFile(assetManager, modelPath)
       val options = Interpreter.Options()
-      options.numThreads = count
+      options.numThreads = count?.toInt() ?: 1
       interpreter = Interpreter(byteFile, options)
       interpreter?.allocateTensors()
-      promise.resolve("initialization tflite success")
+      return "initialization tflite success"
     } catch (e: Exception) {
       e.printStackTrace()
-      promise.reject(Throwable(e))
+      throw Throwable(e.printStackTrace().toString())
     }
   }
 
-  @Throws(IOException::class)
-  private fun loadModelFile(assets: AssetManager, modelFilename: String): MappedByteBuffer {
-    val fileDescriptor = assets.openFd("$modelFilename.tflite")
-    val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
-    val fileChannel = inputStream.channel
-    val startOffset = fileDescriptor.startOffset
-    val declaredLength = fileDescriptor.declaredLength
-    return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
-  }
-
-  @ReactMethod
-  fun detectFromBase64(imageString: String?, promise: Promise) {
+  override fun detectFromBase64(imageString: String): DetectBase64 {
+    // TODO("Not yet implemented")
     try {
       val decodedString = Base64.decode(imageString, Base64.DEFAULT)
       val bmpStorageResult = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
@@ -76,7 +64,7 @@ class VisionCameraFaceDetectionModule(private val reactContext: ReactApplication
       if (faces.size > 0) {
         val face = faces[0]
         val bmpFaceStorage =
-          Bitmap.createBitmap(TF_OD_API_INPUT_SIZE, TF_OD_API_INPUT_SIZE, Bitmap.Config.ARGB_8888)
+          createBitmap(TF_OD_API_INPUT_SIZE, TF_OD_API_INPUT_SIZE)
         val faceBB = RectF(face.boundingBox)
         val cvFace = Canvas(bmpFaceStorage)
         val sx = TF_OD_API_INPUT_SIZE.toFloat() / faceBB.width()
@@ -98,7 +86,7 @@ class VisionCameraFaceDetectionModule(private val reactContext: ReactApplication
         map.putDouble("leftEyeOpenProbability", face.leftEyeOpenProbability?.toDouble() ?: 0.0)
         map.putDouble("rightEyeOpenProbability", face.rightEyeOpenProbability?.toDouble() ?: 0.0)
         map.putDouble("smilingProbability", face.smilingProbability?.toDouble() ?: 0.0)
-        promise.resolve(map)
+        return map
       } else {
         map.putString("message", "No Face")
         map.putArray("data", Arguments.createArray())
@@ -106,19 +94,21 @@ class VisionCameraFaceDetectionModule(private val reactContext: ReactApplication
         map.putDouble("leftEyeOpenProbability", 0.0)
         map.putDouble("rightEyeOpenProbability", 0.0)
         map.putDouble("smilingProbability", 0.0)
-        promise.resolve(map)
+        return map
       }
     } catch (e: Exception) {
       e.printStackTrace()
-      promise.reject(Throwable(e))
+      throw Throwable(e.printStackTrace().toString())
     }
   }
 
-  override fun getName(): String {
-    return NAME
-  }
-
-  companion object {
-    const val NAME = "VisionCameraFaceDetectionModule"
+  @Throws(IOException::class)
+  private fun loadModelFile(assets: AssetManager, modelFilename: String): MappedByteBuffer {
+    val fileDescriptor = assets.openFd("$modelFilename.tflite")
+    val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+    val fileChannel = inputStream.channel
+    val startOffset = fileDescriptor.startOffset
+    val declaredLength = fileDescriptor.declaredLength
+    return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
   }
 }
