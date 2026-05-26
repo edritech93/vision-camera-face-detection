9313bf33b221f7809c82eef45bb01fb429b89a8f
Yudi Alviska <yudi.a@weefer.co.id>
2026-05-26 13:48:54 +0700

    fix: add error handling to tensor initialization and detection methods

--- ios/HybridTensorFactory.swift
+++ ios/HybridTensorFactory.swift
@@ -3,10 +3,10 @@ import VisionCamera
 
 class HybridTensorFactory: HybridTensorFactorySpec {
   func initTensor() throws -> String {
-    return HybridTensor().initTensor()
+    return try HybridTensor().initTensor()
   }
   
   func detectFromBase64(options: TensorFaceOptions) throws -> Variant_NullType__any_HybridFaceSpec_ {
-    return HybridTensor().detectFromBase64(options)
+    return try HybridTensor().detectFromBase64(options: options)
   }
 }
